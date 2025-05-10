393b8f79a88f8f5c59502f6886f01836c583b0d2
Alexi <jemimassyrup@gmail.com>
2025-05-09 21:07:34 -0700

    Split scenes

--- Assets/Scripts/Scenes/Intro/IntroGameManager.cs
+++ Assets/Scripts/Scenes/Intro/IntroGameManager.cs
@@ -4,8 +4,14 @@ public class IntroGameManager : MonoBehaviour
 {
     void Start()
     {
-        DialogueManager.Singleton.StartNarration("ASDW to walk around.", 3);
+        DialogueManager.Singleton.StartNarration("ASDW to walk around.", 6);
+		Invoke(nameof(Other), 14);
     }
+	void Other()
+	{
+        DialogueManager.Singleton.StartNarration("Move mouse to look around.");
+
+	}
 	public void EatApple()
 	{
 		DialogueManager.Singleton.StartNarration("That was delicious. Let's keep exploring");
